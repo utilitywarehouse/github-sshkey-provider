037b11d5ec7daf8af7c33dd1ddd2beb145a4e0da
Dimitrios Karagiannis <dhkarag@gmail.com>
2016-08-04 18:20:02 +0100

    fixed various issues with the agent

--- cmd/agent.go
+++ cmd/agent.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -20,39 +21,58 @@ var agentCmd = &cobra.Command{
 	Short: "starts the agent",
 	Long:  "Will listen for notifications from the collector and adjust the authorized_keys file.",
 	Run: func(cmd *cobra.Command, args []string) {
+		simplelog.Infof("Starting up [agentRecoverInterval=%d]", viper.GetInt("agentRecoverInterval"))
+
 		rt := transporter.NewRedis(
 			viper.GetString("redisHost"),
 			viper.GetString("redisPassword"),
 			viper.GetString("redisChannel"),
 		)
 
+		isActive := true
+		timer := time.NewTimer(time.Nanosecond)
+
 		// handle interrupt
 		sigChannel := make(chan os.Signal, 1)
 		signal.Notify(sigChannel, os.Interrupt)
 		go func() {
 			<-sigChannel
-			simplelog.Infof("Shutdown started, waiting for goroutines to return")
+			simplelog.Infof("Shutdown started, waiting for processes to return")
+			isActive = false
+			timer.Reset(time.Nanosecond)
 			rt.StopListening()
 		}()
 
-		updateAuthorizedKeys(rt)
+		<-timer.C
+		for isActive {
+			if err := rt.Listen(func(message string) error {
+				simplelog.Infof("Updating %s", viper.GetString("authorizedKeysPath"))
+
+				err := gskp.AuthorizedKeys.Update(viper.GetString("authorizedKeysPath"), message)
+				if err != nil {
+					simplelog.Infof("Error occured while trying to update '%s': %v",
+						viper.GetString("authorizedKeysPath"), err)
+				}
+
+				return nil
+			}); err != nil {
+				simplelog.Infof("Listen returned error: %v", err)
+			}
+
+			if isActive {
+				simplelog.Infof("Waiting %d seconds before trying to establish a connection again",
+					viper.GetInt("agentRecoverInterval"))
+
+				timer.Reset(time.Duration(viper.GetInt("agentRecoverInterval")) * time.Second)
+			}
+
+			<-timer.C
+		}
 
 		simplelog.Infof("Shutdown complete, exiting now")
 	},
 }
 
 func updateAuthorizedKeys(rt *transporter.Redis) {
-	if err := rt.Listen(func(message string) error {
-		simplelog.Infof("Updating %s", viper.GetString("authorizedKeysPath"))
 
-		err := gskp.AuthorizedKeys.Update(viper.GetString("authorizedKeysPath"), message)
-		if err != nil {
-			simplelog.Infof("Error occured while trying to update '%s': %v",
-				viper.GetString("authorizedKeysPath"), err)
-		}
-
-		return nil
-	}); err != nil {
-		simplelog.Errorf("Listen error: %v", err)
-	}
 }
