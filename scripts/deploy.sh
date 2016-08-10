b9bd64ced45f572e64cd93b87a5f58938e8a5479
Dimitrios Karagiannis <dhkarag@gmail.com>
2016-08-10 13:20:03 +0100

    changes in the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,21 +3,25 @@
 set -o errexit
 set -o nounset
 
-if [ $# -ne 3 ]; then
-    echo "usage: ./scripts/deploy.sh <kubernetes_url> <repo_name> <git_sha>"
+if [ $# -ne 2 ]; then
+    echo "usage: ./scripts/deploy.sh <repo_name> <git_sha>"
     exit 1
 fi
 
-if [[ -z ${KUBERNETES_TOKEN:-} ]]; then
+if [ -z ${KUBERNETES_URL:-} ]; then
+    echo "could not read the kubernetes url from \$KUBERNETES_URL"
+    exit 1
+fi
+
+if [ -z ${KUBERNETES_TOKEN:-} ]; then
     echo "could not read the kubernetes token from \$KUBERNETES_TOKEN"
     exit 1
 fi
 
-kubernetes_url=$1
-repo_name=$2
-git_sha=$3
+repo_name=$1
+git_sha=$2
 
-function payload() {
+payload () {
     cat <<-PAYLOAD
     {
         "spec": {
@@ -40,4 +44,4 @@ curl -k -XPATCH \
     -d "$(payload collector)" \
     -H "Content-Type: application/strategic-merge-patch+json" \
     -H "Authorization: Bearer ${KUBERNETES_TOKEN}" \
-    "${kubernetes_url}/apis/extensions/v1beta1/namespaces/default/deployments/${repo_name}-collector"
+    "${KUBERNETES_URL}/apis/extensions/v1beta1/namespaces/default/deployments/${repo_name}-collector"
