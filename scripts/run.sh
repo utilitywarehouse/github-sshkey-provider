d00293b21a2bbe4bf7f125fcaa9bf0dfa373d13c
Dimitrios Karagiannis <dhkarag@gmail.com>
2016-08-10 11:01:22 +0100

    building inside containers

---
+++ scripts/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [  $# -ne 1 ]; then
+    echo "usage: ./scripts/run.sh <script>"
+    echo "   eg: ./scripts/run.sh test_and_build"
+    exit 1
+fi
+
+if [ ! -x scripts/$1.sh ]; then
+   echo "invalid script: ${1}"
+   exit 1
+fi
+
+build_image_name=golang:1.6.3-alpine
+build_image_gopath=/go/src
+package_name=github.com/utilitywarehouse/github-sshkey-provider
+
+docker run -it --rm \
+    -v $(pwd):${build_image_gopath}/${package_name} \
+    -w ${build_image_gopath}/${package_name} \
+    ${build_image_name} ./scripts/${1}.sh
---
+++ scripts/test_and_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+apk add --no-cache curl git redis
+
+curl -sS https://glide.sh/get | sh
+
+glide i
+
+redis-server --port 6379 --daemonize yes
+redis-server --port 6380 --requirepass password --daemonize yes
+
+go test -tags integration -cover $(glide novendor)
+
+CGO_ENABLED=0 go build -v -a -ldflags '-s -extldflags "-static"'
